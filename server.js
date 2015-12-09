4c7353c3e10a7d945bab1ffaf29cefce436fe61a
GARETH COUCH <gcouch8@gmail.com>
2015-12-08 20:33:27 -0800

    images commit

---
+++ server.js
@@ -0,0 +1,465 @@
+queenAnne: [
+  {
+    id: 'seattleCenter',
+    name: 'Seattle Center',
+    cost: 1,
+    type: 'attraction',
+    outdoor: true,
+    kidfriendly: true,
+    url:'http://www.seattlecenter.com/'
+  },
+  {
+    id: 'chihulyGlass',
+    name: 'Chihuly Garden and Glass',
+    cost: 2,
+    type: 'museum',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.chihulygardenandglass.com/'
+  },
+  {
+    id: 'keyArena',
+    name: 'KeyArena',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.keyarena.com/'
+  },
+  {
+    id: 'hulaHula',
+    name: 'Hula Hula',
+    cost: 2,
+    type: 'bar',
+    outdoor: false,
+    kidfriendly: false,
+    url:'http://hulahula.org/'
+  },
+  {
+    id: 'spaceNeedle',
+    name: 'Space Needle',
+    cost: 2,
+    type: 'attraction',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.spaceneedle.com/home/'
+  },
+  {
+    id: 'emp',
+    name: 'EMP',
+    cost: 2,
+    type: 'museum',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.empmuseum.org/'
+  },
+  {
+    id: 'pacificScienceCenter',
+    name: 'Pacific Science Center',
+    cost: 1,
+    type: 'museum',
+    outdoor: false,
+    kidfriendly: true,
+    url:'https://www.pacificsciencecenter.org/'
+  },
+  {
+    id: 'kerryPark',
+    name: 'Kerry Park',
+    cost: 1,
+    type: 'park',
+    outdoor: true,
+    kidfriendly: true,
+    url:'http://www.seattle.gov/parks/park_detail.asp?ID=342'
+  },
+  {
+    id: 'qaBeerhall',
+    name: 'Queen Anne Beerhall',
+    cost: 1,
+    type: 'bar',
+    outdoor: false,
+    kidfriendly: false,
+    url:'http://www.queenannebeerhall.com/'
+  },
+  {
+    id: 'uptEspresso',
+    name: 'Uptown Espresso',
+    cost: 1,
+    type: 'cafe',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.velvetfoam.com/'
+  },
+  {
+    id: 'elDiablo',
+    name:'El Diablo Coffe Co',
+    cost: 1,
+    type: 'cafe',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.eldiablocoffee.com/'
+  },
+  {
+    id:'caffeLadro',
+    name: 'Caffe Ladro',
+    cost: 1,
+    type: 'cafe',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.caffeladro.com/'
+  },
+  {
+    id:'toulousePetite',
+    name: 'Toulouse Petite',
+    cost: 2,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: false,
+    url:'http://toulousepetit.com/'
+  },
+  {
+    id:'metMarket',
+    name:'Metropolitan Market',
+    cost: 2,
+    type: 'grocery',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'https://metropolitan-market.com/home.php?'
+  },
+  {
+    id: 'mEdPark',
+    name: 'Myrtle Edwards Park',
+    cost: 1,
+    type: 'park',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://www.seattle.gov/parks/park_detail.asp?ID=311'
+  },
+  {
+    id: 'qaLibrary',
+    name: 'Queen Anne Public Library',
+    cost: 1,
+    type: 'library',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.spl.org/locations/queen-anne-branch'
+  },
+  {
+    id: 'chocolopolis',
+    name: 'Chocolopolis',
+    cost: 1,
+    type: 'shopping',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.chocolopolis.com/'
+  },
+  {
+    id: 'macrinaBakery',
+    name: 'Macrina Bakery',
+    cost: 1,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.macrinabakery.com/'
+  },
+  {
+    id: 'meccaCafe',
+    name: 'The Mecca Cafe',
+    cost: 1,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://mecca-cafe.com/'
+  },
+  {
+    id:'cederbergTeaHouse',
+    name: 'Cederberg Tea House',
+    cost: 1,
+    type: 'cafe',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://cederbergteahouse.com/'
+  },
+  {
+    id:'howToCookaWolf',
+    name: 'How to Cook a Wolf',
+    cost: 2,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://www.ethanstowellrestaurants.com/locations/how-to-cook-a-wolf/'
+  },
+  {
+    id: 'mezcaleriaOaxaca',
+    name: 'Mezcaleria Oaxaca',
+    cost: 2,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url:'http://www.mezcaleriaoaxaca.com/MO_QueenAnne/MO_QAnne_home.html'
+  },
+  {
+    id:'canlis',
+    name: 'Canlis',
+    cost: 3,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: false,
+    url:'http://canlis.com/'
+  }
+],
+
+downtown: [
+  {
+    id: 'cinerama',
+    name: 'Cinerama',
+    cost: 1,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'https://www.cinerama.com/'
+  },
+  {
+    id:'steinbrueckGallery',
+    name: 'Steinbrueck Native Gallery',
+    cost: 3,
+    type: 'museum',
+    outdoor: false,
+    kidfriendly: false,
+    url:'http://www.steinbruecknativegallery.com/'
+  },
+  {
+    id: 'westLakeShoppingCenter',
+    name: 'Westlake Shopping Center',
+    cost: 2,
+    type: 'shopping',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.westlakecenter.com/en.html'
+  },
+  {
+    id:'pikePlaceMarket',
+    name:'Pike Place Market',
+    cost: 1,
+    type: 'attraction',
+    outdoor: true,
+    kidfriendly: true,
+    url:'http://pikeplacemarket.org/'
+  },
+  {
+    id: 'olympicSculpturePark',
+    name: 'Olympic Sculpture Park',
+    cost: 1,
+    type: 'park',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://www.seattleartmuseum.org/visit/olympic-sculpture-park'
+  },
+  {
+    id: 'safeCoField',
+    name: 'SafeCo Field',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: true,
+    kidfriendly: true,
+    url:'http://seattle.mariners.mlb.com/sea/ballpark/'
+  },
+  {
+    id: 'centuryLinkField',
+    name: 'CenturyLink Field',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://www.centurylinkfield.com/'
+  },
+  {
+    id:'waterFront',
+    name: 'The Seattle Waterfront',
+    cost: 1,
+    type: 'park',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://www.seattle.gov/TOUR/water.htm'
+  },
+  {
+    id: 'ferrisWheel',
+    name: 'Seattle Great Wheel',
+    cost: 1,
+    type: 'attraction',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://seattlegreatwheel.com/'
+  },
+  {
+    id: 'columbiaCenter',
+    name: 'Columbia Center',
+    cost: 1,
+    type: 'attraction',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.skyviewobservatory.com/'
+  },
+  {
+    id: 'ferryTerminal',
+    name: 'Seattle Ferry Terminal',
+    cost: 1,
+    type: 'attraction',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.wsdot.wa.gov/ferries/'
+  },
+  {
+    id: 'benaroyaHall',
+    name: 'Benaroya Hall',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.seattlesymphony.org/benaroyahall'
+  },
+  {
+    id: 'umiSakeHouse',
+    name: 'Umi Sake House',
+    cost: 2,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://www.umisakehouse.com/'
+  },
+  {
+    id: 'sam',
+    name: 'Seattle Art Museum',
+    cost: 1,
+    type: 'museum',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.seattleartmuseum.org/'
+  },
+  {
+    id: 'seattleAquarium',
+    name: 'Seattle Aquarium',
+    cost: 2,
+    type: 'attraction',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.seattleaquarium.org/'
+  },
+  {
+    id: 'pikeBrewingCo',
+    name: 'The Pike Brewing Company',
+    cost: 1,
+    type: 'bar',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://pikebrewing.com/'
+  },
+  {
+    id: 'steelheadDiner',
+    name: 'Steelhead Dinner',
+    cost: 2,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.steelheaddiner.com/'
+  },
+  {
+    id: 'piroshkyPiroshky',
+    name: 'Piroshky Piroshky',
+    cost: 1,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'https://www.piroshkybakery.com/'
+  },
+  {
+    id: 'showbox',
+    name: 'The Showbox',
+    cost: 2,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://www.showboxpresents.com/'
+  },
+  {
+    id: 'paramount',
+    name: 'The Paramount Theatre',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.stgpresents.org/paramount'
+  },
+  {
+    id: 'crocodile',
+    name: 'The Crocodile',
+    cost: 2,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://www.thecrocodile.com/'
+  },
+  {
+    id: 'moore',
+    name: 'The Moore Theatre',
+    cost: 3,
+    type: 'entertainment',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'http://www.stgpresents.org/moore'
+  },
+  {
+    id: 'seattlePublicLibrary',
+    name: 'Seattle Public Library',
+    cost: 1,
+    type: 'library',
+    outdoor: false,
+    kidfriendly: false,
+    url: 'https://www.spl.org/locations/central-library'
+  },
+  {
+    id: 'undergroundTour',
+    name: 'Underground Tour',
+    cost: 1,
+    type: 'attraction',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.undergroundtour.com/'
+  },
+  {
+    id: 'pioneerSquare',
+    name: 'Pioneer Square',
+    cost: 1,
+    type: 'attraction',
+    outdoor: true,
+    kidfriendly: true,
+    url: 'http://www.pioneersquare.org/'
+  },
+  {
+    id: 'scw',
+    name: 'Seattle Coffee Works',
+    cost: 1,
+    type: 'cafe',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://www.seattlecoffeeworks.com/'
+  },
+  {
+    id: 'biscuitBitch',
+    name: 'Biscuit Bitch',
+    cost: 1,
+    type: 'restaurant',
+    outdoor: false,
+    kidfriendly: true,
+    url: 'http://biscuitbitch.com/'
+  }
+]
+
+
+app.get('/ballard/:id', function(req, res){
+  var id = req.params.id;
+  db.ballard.forEach(function(thing){
+    if(thing.id === id){
+      res.json(thing);
+    }
+  });
+});
